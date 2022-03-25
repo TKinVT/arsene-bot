239067224457ebef024682886acd4ccac4c56410
TK <tk@tkinvt.com>
2022-03-25 15:24:03 -0400

    added new module for twitter pouting

---
+++ arsenal/twitter_profile.py
@@ -0,0 +1,24 @@
+import os
+
+import tweepy
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+CONSUMER_KEY = os.getenv('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('TWITTER_CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('TWITTER_TOKEN_SECRET')
+BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
+
+
+def manage_account():
+    client = tweepy.Client(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+    return client
